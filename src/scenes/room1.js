87ae550b403d37d8f258c550bd431b7feaa5fdb7
Roger Correa <136924837+rmeIIo@users.noreply.github.com>
2025-03-31 18:58:50 -0300

    feat: implementing logic to load and display the map

--- src/scenes/room1.js
+++ src/scenes/room1.js
@@ -1,1 +1,16 @@
-export function room1(k) {}
+import { setBackgroundColor } from "./roomUtils.js";
+
+export function room1(k, roomData) {
+  setBackgroundColor(k, "#a2aed5");
+
+  const roomLayers = roomData.layers;
+
+  const map = k.add([k.pos(0, 0), k.sprite("room1")]);
+  const colliders = [];
+  for (const layer of roomLayers) {
+    if (layer.name === "colliders") {
+      colliders.push(...layer.objects);
+      break;
+    }
+  }
+}
---
+++ src/scenes/roomUtils.js
@@ -0,0 +1,7 @@
+export function setBackgroundColor(k, hexColorCode) {
+  k.add([
+    k.rect(k.width(), k.height()),
+    k.color(k.Color.fromHex(hexColorCode)),
+    k.fixed(),
+  ]);
+}
