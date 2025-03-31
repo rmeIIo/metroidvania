d29d7bf739f0f173f2444cd0466c781c6fe536dc
Roger Correa <136924837+rmeIIo@users.noreply.github.com>
2025-03-31 18:32:36 -0300

    feat: defining scenes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import { k } from "./kaboomLoader.js";
+import { room1 } from "./scenes/room1.js";
+import { room2 } from "./scenes/room2.js";
+
+k.scene("room1", () => {
+  room1(k);
+});
+
+k.scene("room2", () => {
+  room2();
+});
+
+k.scene("intro", () => {});
+
+k.go("intro");
