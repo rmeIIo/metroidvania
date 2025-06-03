d6d88b1ed3bcb5606fa534667bca90f48dec911f
Roger Correa <136924837+rmeIIo@users.noreply.github.com>
2025-06-02 21:14:32 -0300

    feat: Implementing drone enemy

--- src/scenes/room1.js
+++ src/scenes/room1.js
@@ -1,3 +1,4 @@
+import { makeDrone } from "../entities/enemyDrone.js";
 import { makePlayer } from "../entities/player.js";
 import {
   setBackgroundColor,
@@ -47,6 +48,13 @@ export function room1(k, roomData) {
       player.setControls();
       player.setEvents();
       player.enablePassthrough();
+      continue;
+    }
+
+    if (position.type === "drone") {
+      const drone = map.add(makeDrone(k, k.vec2(position.x, position.y)));
+      drone.setBehavior();
+      drone.setEvents();
     }
   }
 }
